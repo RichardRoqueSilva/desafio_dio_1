a362f255cfe374dfda06df5c5d2ab0ea1e2a43e5
RichardRoqueSilva <richard--silva@hotmail.com>
2025-11-03 11:33:56 -0300

    [FIX] - Criação do primeiro app, com Translations Editor

---
+++ app/src/main/java/com/example/meuprimeiroappdio/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.meuprimeiroappdio
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.meuprimeiroappdio.ui.theme.MeuPrimeiroAppDIOTheme
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.stringResource
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MeuPrimeiroAppDIOTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Greeting(
+                            name = "Richard"
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = stringResource(R.string.saudacao_inicial, name),
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MeuPrimeiroAppDIOTheme {
+        Greeting("Android")
+    }
+}
